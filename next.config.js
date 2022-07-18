0ff1ae458b4722bc177777d372a6537a90ae4dd9
tai <nguyentientai10@gmail.com>
2022-07-18 18:49:08 +0700

    fix deploy

--- next.config.js
+++ next.config.js
@@ -14,15 +14,15 @@ module.exports = {
     CLOUD_API: "https://api.cloudinary.com/v1_1/taithinhnam/image/upload",
   },
 };
-module.exports = {
-  env: {
-    BASE_URL: process.env.BASE_URL,
-    MONGODB_URL: process.env.MONGODB_URL,
-    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
-    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
-    PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
-    CLOUD_UPDATE_PRESET: process.env.CLOUD_UPDATE_PRESET,
-    CLOUD_NAME: process.env.CLOUD_NAME,
-    CLOUD_API: process.env.CLOUD_API,
-  },
-};
+// module.exports = {
+//   env: {
+//     BASE_URL: process.env.BASE_URL,
+//     MONGODB_URL: process.env.MONGODB_URL,
+//     ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+//     REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+//     PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
+//     CLOUD_UPDATE_PRESET: process.env.CLOUD_UPDATE_PRESET,
+//     CLOUD_NAME: process.env.CLOUD_NAME,
+//     CLOUD_API: process.env.CLOUD_API,
+//   },
+// };
